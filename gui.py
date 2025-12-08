65dd64e0cba2930125bf02e0b79d47f6a32d8671
Daniel Moreno <danielmoreno@daniels-macbook-air-2.home>
2025-12-08 06:20:27 +0100

    Implemented single cycle option, and improved the design of the output text box.

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@ from ttkthemes import ThemedTk
 from main import SantaTime
 import random
 
+
 # themes and main labels and such
 root = ThemedTk(theme='yaru')
 root.geometry("1000x700")
@@ -24,6 +25,7 @@ checks_frame.pack(anchor='nw', fill='y')
 
 right_frame = ttk.Frame(main_frame)
 right_frame.pack(side='right', fill='both', expand=True, padx=10, pady=10)
+
 
 # checkbuttons
 def _make_checkbutton(committee):
@@ -52,6 +54,7 @@ buttons_dict = {}
 
 for committee in committee_list:
     _make_checkbutton(committee)
+
 
 # Add Other section
 other_label = ttk.Label(left_frame, text='Add Other: ')
@@ -91,24 +94,54 @@ add_button = ttk.Button(
 add_button.pack(anchor='w', padx=20, pady=(5, 10))
 
 
+# toggle button for single cycle option
+toggle_var = tk.IntVar()
+scl_toggle = tk.Checkbutton(
+        left_frame,
+        text="Single Continuous Loop (Recommended)",
+        variable=toggle_var,
+        wraplength=200,
+        indicatoron=False,
+        borderwidth = 4,
+        foreground = "#772953",
+        selectcolor = "#772953"
+    )
+scl_toggle.invoke()
+scl_toggle.pack(anchor='w', padx=20)
+
+
 # result box
-results_box = tk.Text(right_frame, height=25, width=50, font=("PT Mono", 20))
+results_box = tk.Text(
+    right_frame, 
+    height=25, 
+    width=50, 
+    font=("PT Mono", 20), 
+    relief="groove",
+    selectbackground="#7E5C6E", 
+    inactiveselectbackground="#71666C",
+    highlightthickness=1.5
+)
+
+results_box.config(highlightbackground="#928d91", highlightcolor="#928d91")
+
 results_box.tag_configure("center", justify='center')
 
-# calls main.py logic
 def shuffle():
+    """Calls main.py logic. Applied when main button is pressed."""
     selected = []
     for committee in committee_list:
         key = committee.lower().replace(" ", "_")
         if vars_dict[key].get() == 1:
             selected.append(committee)
     santa = SantaTime(selected)
-    santa.shuffle_until_valid()
+    if toggle_var.get() == 1:
+        santa.single_cycle_shuffle()
+    else:
+        santa.shuffle_until_valid()
     result_string = santa.return_string_results() 
     results_box.delete("1.0", tk.END)
     results_box.insert(tk.END, result_string, "center")
 
-# big button calls shuffle()
 main_button = ttk.Button(right_frame,
                         text ='Reveal Setup!',
                         command=shuffle, 
